c3ed7ea583098daa283dcca48b8d2a6877c920de
toledanogus <toledanogu@gmail.com>
2023-04-05 08:09:49 -0600

    Primero

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgm8RwLRz_lm1rZsjGN5xD0L7YzLS4VB0",
+  authDomain: "primeroproyecto-52fe6.firebaseapp.com",
+  projectId: "primeroproyecto-52fe6",
+  storageBucket: "primeroproyecto-52fe6.appspot.com",
+  messagingSenderId: "786405075858",
+  appId: "1:786405075858:web:e3314ceb2b8c9e92332f4b"
+};
+
+// Initialize Firebase
+
+
+
 
 function App() {
+
+  const app = initializeApp(firebaseConfig);
+
+  const [datos, setDatos] = useState('');
+  
+  
+  // Get a list of cities from your database
+  async function getCities(db) {
+    const citiesCol = collection(getFirestore(app), 'alumnos');
+    const citySnapshot = await getDocs(citiesCol);
+    const cityList = citySnapshot.docs.map(doc => doc.data());
+    console.log(cityList[1].nombre);
+setDatos(cityList[1].nombre);
+    return cityList;
+  }
+
+  useEffect(() => {
+    getCities();
+  }, []);
+  
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Gustavo</h1>
+    <div>{datos}</div>
+    </>
   );
 }
 
